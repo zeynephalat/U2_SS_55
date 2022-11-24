fb256bcdf404ac97cddb4df57e6f2900aee05b65
SBEML\amp-1128 <zeynephalat53@gmail.com>
2022-11-24 11:12:17 +0300

    Add project files.

---
+++ U2_SS_55/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_SS_55
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double boykilo;
+            boykilo = Convert.ToDouble(textBox1.Text);
+            if (boykilo>25 && boykilo <40)
+            {
+                MessageBox.Show("zayıf");
+            }
+            else if (boykilo >50 && boykilo <70)
+            {
+                MessageBox.Show("normal");
+            }
+            else
+            {
+                MessageBox.Show("kilolu");
+            }
+        }
+    }
+}
